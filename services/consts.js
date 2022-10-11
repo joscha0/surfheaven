c3e74d41de194a0b967f733331acd061ab50716c
Joscha von Andrian <joschavonandrian@gmail.com>
2022-10-11 16:21:10 +0200

    add mapnamefixes api

--- services/consts.js
+++ services/consts.js
@@ -1,3 +1,8 @@
 const BASE_URL = "https://surfheaven.eu/api/";
 
-export { BASE_URL };
+const mapnameFixes = {
+  surf_network2008_final: "surf_network_2008_final",
+  surf_aircontrol_ksf_b2: "surf_aircontrol_ksf_b1",
+};
+
+export { BASE_URL, mapnameFixes };
